d6cf9a57b71e73a067705f9dfe31754c1b16ad96
mrou <820823707@qq.com>
2025-03-15 16:48:37 +0800

    Update PUSHPLUS.py

--- PUSHPLUS.py
+++ PUSHPLUS.py
@@ -1,6 +1,6 @@
 import requests
 import os
-token = os.environ['TOKEN']
+token = os.environ['51128351c4004a8288c26a6a4a489153']
 title= 'Epic-FreeGamer'
 content ='Epic-FreeGamer任务已执行'
 url = 'http://www.pushplus.plus/send?token='+token+'&title='+title+'&content='+content
